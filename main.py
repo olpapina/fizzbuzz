2470c01d58c0eb6e5ec6f1a45aaf94c5a53f7d2a
olpapina <opapina@solvd.com>
2023-06-05 17:10:28 +0300

    fizzbuzz is rewritten in two lines

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
 num = int(input("Enter any integer number: "))
-
-
-def fizzbuzz(number):
-    rem3 = number % 3
-    rem5 = number % 5
-    if not rem3 and not rem5:
-        return "FizzBuzz"
-    elif not rem5:
-        return "Buzz"
-    elif not rem3:
-        return "Fizz"
-    else:
-        return number
-
-
-for n in range(1, num + 1):
-    print(fizzbuzz(n))
+print('\n'.join('Fizz' * (i%3==0) + 'Buzz' * (i%5==0) or str(i) for i in range(1, num+1)))
